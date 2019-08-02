500acbbd01b2846d338ed48186ab206d421303c5
donvardix <donvardix@gmail.com>
2019-08-02 11:32:30 +0300

    improved code

--- my_scripts/php/old/steam/krit_dota.php
+++ my_scripts/php/old/steam/krit_dota.php
@@ -1,31 +1,22 @@
 <?php
-//Вычисляет примерное количество критов подряд с 6 предметов
-$chance = 30; //Процент крита
-$while = 0;
-$step = 0; //Количество критов подряд
-function crit($chance)
-{
-    if (rand(1, 100) <= $chance) {
-        return false;
-    } else {
-        return true;
-    }
-}
+/**
+ * Вычисляет примерное количество критов подряд с 6 предметов
+ */
 
-while ($while < 1) {
-    $step++;
-    if (crit($chance)) {
-        if (crit($chance)) {
-            if (crit($chance)) {
-                if (crit($chance)) {
-                    if (crit($chance)) {
-                        if (crit($chance)) {
-                            $while = 1;
-                        }
-                    }
-                }
-            }
+echo crit(30, 6);
+
+function crit($chance, $countItem)
+{
+    $count = 0;
+    $step = 1;
+    while ($step <= $countItem) {
+        if (rand(1, 100) <= $chance) {
+            $count++;
+            $step = 1;
+            continue;
+        } else {
+            $step++;
         }
     }
+    return $count;
 }
-echo $step;
